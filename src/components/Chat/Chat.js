22afb8b12aea6d690dcab52b10b0799e9d9e8140
Abinash Nayak <abinash.nayak1712@gmail.com>
2020-08-28 13:21:08 +0530

    Show active members on Tab/Mobile View

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,6 +7,9 @@ import Messages from "../Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 
+import onlineIcon from "../../icons/onlineIcon.png";
+import memberIcon from "../../icons/memberIcon.png";
+
 import "./Chat.css";
 
 let socket;
@@ -17,6 +20,7 @@ const Chat = ({ location, ...props }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [showMembers, setShowMembers] = useState(false);
   const ENDPOINT = "https://backend-chat-application-sio.herokuapp.com/";
 
   useEffect(() => {
@@ -33,7 +37,7 @@ const Chat = ({ location, ...props }) => {
         props.history.push("/");
       }
     });
-  }, [ENDPOINT, location.search]);
+  }, [ENDPOINT, location.search, props.history]);
 
   useEffect(() => {
     socket.on("message", (message) => {
@@ -53,17 +57,54 @@ const Chat = ({ location, ...props }) => {
     }
   };
 
+  const handleShowMembers = () => {
+    setShowMembers(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowMembers(false);
+  };
+
   return (
     <div className="outerContainer">
-      <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
-      </div>
+      {showMembers ? (
+        <div className="container">
+          <InfoBar
+            handleCloseClick={handleCloseClick}
+            room={"Active Members"}
+            membersView={true}
+          />
+          <div className="activeMembersOuterContainer">
+            <div className="activeMembersContainer">
+              {users.map(({ name }) => (
+                <div key={name} className="activeMemberItem">
+                  <img
+                    alt="Member Icon"
+                    src={memberIcon}
+                    style={{ height: "40px", width: "40px", marginRight: "2%" }}
+                  />
+                  {name}
+                  <img
+                    alt="Online Icon"
+                    src={onlineIcon}
+                    style={{ marginLeft: "2%" }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="container">
+          <InfoBar handleShowMembers={handleShowMembers} room={room} />
+          <Messages messages={messages} name={name} />
+          <Input
+            message={message}
+            setMessage={setMessage}
+            sendMessage={sendMessage}
+          />
+        </div>
+      )}
       <TextContainer users={users} />
       <div className="footer">
         <span>
--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import onlineIcon from '../../icons/onlineIcon.png';
-import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+import faceIcon from "../../icons/face.png";
 
-import './InfoBar.css';
+import "./InfoBar.css";
 
-const InfoBar = ({ room }) => (
-  <div className="infoBar">
-    <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-      <h3>{room}</h3>
+const InfoBar = ({
+  room,
+  handleShowMembers,
+  membersView,
+  handleCloseClick,
+}) => {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+        <h3>{room}</h3>
+        {!membersView && (
+          <img
+            onClick={handleShowMembers}
+            className="faceIcon"
+            src={faceIcon}
+            alt="online icon"
+          />
+        )}
+      </div>
+      <div className="rightInnerContainer">
+        {membersView ? (
+          <img
+            className="closeIcon"
+            onClick={handleCloseClick}
+            src={closeIcon}
+            alt="close icon"
+          />
+        ) : (
+          <a href="/">
+            <img src={closeIcon} alt="close icon" />
+          </a>
+        )}
+      </div>
     </div>
-    <div className="rightInnerContainer">
-      <a href="/"><img src={closeIcon} alt="close icon" /></a>
-    </div>
-  </div>
-);
+  );
+};
+
+InfoBar.defaultProps = {
+  room: "Virtual Room",
+  handleShowMembers: () => {},
+  handleCloseClick: () => {},
+  membersView: false,
+};
 
 export default InfoBar;
